60bae8c4df93f7de3b233b38191460699c32b570
Viola8905 <74563100+Viola8905@users.noreply.github.com>
2021-03-20 10:44:13 +0200

    Add files via upload

---
+++ lab7.1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace lab7._1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Write("Кількість рядків: ");
+            int r = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Кількість стовпців: ");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            int[,] matrix = new int[r, c];
+            Random random = new Random(DateTime.Now.Millisecond);
+
+            for (int i = 0; i < matrix.GetLength(0); i++) //Як змінюються номери рядків
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++) //Як змінюються номери стовпців
+                {
+                    matrix[i, j] = random.Next(-10, 10);
+                }
+            }
+
+            int result = 0;
+            int[,] matrix2 = new int[r, c];
+            for (int i = 0; i < matrix.GetLength(0); i++) //Як змінюються номери рядків
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++) //Як змінюються номери стовпців
+                {
+                    if (matrix[i, j] < 0 && (i + j) % 2 == 0)
+                    {
+                        matrix2[i, j] = matrix[i, j];
+                    }
+                }
+            }
+            for (int i = 0; i < matrix2.GetLength(0); i++) //Як змінюються номери рядків
+            {
+                for (int j = 0; j < matrix2.GetLength(1); j++) //Як змінюються номери стовпців
+                {
+
+                    result += matrix2[i, j];
+                }
+            }
+            Console.Write("suma=", result);
+        }
+    }
+}
---
+++ lab7.2.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace lab7._2
+{
+    class Program
+    {
+        static void printvec(double[] arr)
+        {
+            for (int j = 0; j < arr.GetLength(0); j++) //Як змінюються номери стовпців
+            {
+                Console.Write("{0,7:f}  ", arr[j]);
+            }
+            Console.WriteLine();
+        }
+        static void printMatrix(double[,] arr)
+        {
+            for (int i = 0; i < arr.GetLength(0); i++) //Як змінюються номери рядків
+            {
+                for (int j = 0; j < arr.GetLength(1); j++) //Як змінюються номери стовпців
+                {
+                    Console.Write("{0,7:f}  ", arr[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("введіть розмірність квадратної матриці: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            double[,] a = new double[n, n];
+            for (int i = 0; i < a.GetLength(0); i++) //Як змінюються номери рядків
+            {
+                for (int j = 0; j < a.GetLength(1); j++) //Як змінюються номери стовпців
+                {
+                    if ((i + 1 + j + 1) % 2 == 0)
+                    {
+                        a[i, j] = a[i, j] + i + 1;
+                    }
+                    else
+                    {
+                        a[i, j] = a[i, j] + Math.Pow(j + 1, 2);
+                    }
+                }
+            }
+            printMatrix(a);
+
+            double[] b = new double[a.GetLength(0)];
+            double dob = 1;
+
+            for (int i = 0; i < n; i++) //Як змінюються номери рядків
+            {
+
+                for (int j = 0; j < n; j++) //Як змінюються номери стовпців
+                {
+                    dob *= a[j, i];
+                }
+                b[i] = dob;
+                dob = 1;
+            }
+            printvec(b);
+            /*
+            double tmp = 1;        // временная переменная для храненя проезведения элементов строки
+            // добавление новых значений в результирующий массив
+            for (int i = 1; i < a.Length; i++)
+            {
+                tmp = 1;
+                for (int j = 1; j < a.Length; j++)
+                {
+                    tmp *= a[j, i];
+                }
+                b[i] = tmp;
+            }
+            */
+
+            Console.WriteLine("вивід масиву:");
+            for (int i = 0; i < b.Length; i++)
+            {
+                Console.WriteLine(b[i]);
+            }
+            Console.ReadLine();
+
+            double maxValue = b[0];
+
+            for (int i = 1; i < b.Length; i++)
+            {
+                if (b[i] < maxValue && b[i] % 2 == 0)
+                {
+                    maxValue = b[i];
+
+                }
+
+            }
+
+            Console.WriteLine(maxValue);
+            Console.ReadLine();
+        }
+
+
+
+
+    }
+}
