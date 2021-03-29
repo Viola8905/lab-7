dfd915c99c82745766fa7ea9462056facbadb010
Viola8905 <74563100+Viola8905@users.noreply.github.com>
2021-03-29 22:37:59 +0300

    Add files via upload

---
+++ lab7.3.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace lab77._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("розмірність матриці: ");
+            int r = Convert.ToInt32(Console.ReadLine());
+
+            int[,] matrix = new int[r, r];
+            Random random = new Random(DateTime.Now.Millisecond);
+
+            for (int i = 0; i < matrix.GetLength(0); i++) //Як змінюються номери рядків
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++) //Як змінюються номери стовпців
+                {
+                    matrix[i, j] = random.Next(-10, 10);
+                }
+            }
+            int[] b = new int[r];
+            for(int i = 0; i < b.Length; i++)
+            {
+                b[i] = random.Next(-10, 10);
+            }
+
+            for (int i = 0; i < matrix.GetLength(0); i++) //Як змінюються номери рядків
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++) //Як змінюються номери стовпців
+                {
+
+                    matrix[i, j] *= b[j];
+                }
+            }
+            int[] c = new int[r];
+
+            for (int i = 0; i < r; i++)
+            {
+                int summa = 0;
+                for (int j = 0; j < r; j++)
+                {
+                    summa += matrix[i, j];
+                }
+                c[i] = summa;
+            }
+            Console.WriteLine(c);
+        }
+    }
+}
